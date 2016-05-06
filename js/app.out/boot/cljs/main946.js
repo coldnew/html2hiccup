64a01029bd5eca5dc623aaa7132cf90433c12b3e
CI-Server <coldnew.tw@gmail.com>
2016-05-06 09:42:34 +0800

    Deploy commit d418a12 to GitHub Pages: Fri May  6 09:42:33 CST 2016

--- js/app.out/boot/cljs/main946.js
+++ js/app.out/boot/cljs/main946.js
@@ -1,5 +1,5 @@
 // Compiled by ClojureScript 1.8.51 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main807');
+goog.provide('boot.cljs.main946');
 goog.require('cljs.core');
 goog.require('html2hiccup.app');
 html2hiccup.app.init();
